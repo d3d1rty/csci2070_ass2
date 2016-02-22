73e0f57fabccdbf37ce16b27fb93c5381a95240b
Dick Davis <rdavis89@my.apsu.edu>
2016-02-22 09:41:55 -0600

    revised exception handling

--- RichardDavisLetterCounter.java
+++ RichardDavisLetterCounter.java
@@ -8,43 +8,40 @@ import java.io.*;
 
 public class RichardDavisLetterCounter
 {
-  public static void main(String args[])
+  public static void main(String args[]) throws IOException
   {
-    String fileName;
-    char target;
-    int numTargets = 0;
+    String fileName; // user supplied file name
+    char target; // the character to look for
+    int numTargets = 0; // the number of targets found
     Scanner in = new Scanner(System.in);
 
     // gets file name from user
     System.out.print("Enter the file name -> ");
     fileName = in.nextLine();
+    File inputFile = new File(fileName);
+    Scanner text = new Scanner(inputFile);
 
     // gets target character from user, truncates extra characters
     System.out.print("Enter the target character -> ");
     target = in.nextLine().charAt(0);
 
-    // tries to open specified file, catches FNF exception
-    try {
-      File inputFile = new File(fileName);
-      Scanner text = new Scanner(inputFile);
-      // continues looping until EOF
-      while (text.hasNext()) {
-        String textLine = text.nextLine();
-        // traverses string, increments numTargets if target found
-        for (int i = 0; i < textLine.length(); i++) {
-          if (textLine.charAt(i) == target) {
-            numTargets++;
-          }
+    // continues looping until EOF
+    while (text.hasNext()) {
+      String textLine = text.nextLine();
+      // traverses string, increments numTargets if target found
+      for (int i = 0; i < textLine.length(); i++) {
+        if (textLine.charAt(i) == target) {
+          numTargets++;
         }
       }
-    } catch (FileNotFoundException e) {
-      System.out.println(e.getMessage());
-      System.exit(0);
     }
 
     // displays results
     System.out.printf("The character %c occurs %d times in file %s\n",
         target, numTargets, fileName);
+
+    // closes file
+    text.close();
 
     // terminates program
     System.exit(0);
