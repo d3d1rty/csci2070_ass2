99745fb19097bdd453b25b7c62cf4e0fc16aad0f
Dick Davis <rdavis89@my.apsu.edu>
2016-02-22 09:13:37 -0600

    commented q1

--- RichardDavisShipping.java
+++ RichardDavisShipping.java
@@ -9,19 +9,22 @@ public class RichardDavisShipping
 {
   public static void main(String args[])
   {
-    double rateOne = 1.10;
-    double rateTwo = 2.20;
-    double rateThree = 3.70;
-    double rateFour = 3.80;
+    double rateOne = 1.10; // rate for lightest package
+    double rateTwo = 2.20; // rate for light package
+    double rateThree = 3.70; // rate for medium package
+    double rateFour = 3.80; // rate for heavy package
     double distance, weight, charges;
     Scanner in = new Scanner(System.in);
 
+    // prompts user for distance in miles
     System.out.print("Enter the distance in miles -> ");
     distance = in.nextDouble();
 
+    // prompts user for weight of package
     System.out.print("Enter the weight of the package -> ");
     weight = in.nextDouble();
 
+    // performs calculation for cost based on weight
     if (weight <= 2) {
       charges = ((int)distance / 500) * rateOne;
     } else if (weight > 2 && weight <= 6) {
@@ -32,8 +35,10 @@ public class RichardDavisShipping
       charges = ((int)distance / 500) * rateFour;
     }
 
+    // displays results
     System.out.printf("Shipping charges: $%6.2f\n", charges);
 
+    // terminates program
     System.exit(0);
   }
 }
